731e3ef2262f384326e5ea07d82874010c0367af
jpminga <jpminga2@utpl.edu.ec>
2023-11-22 16:43:35 -0500

    cambio

--- ejemplos-while/EjemploCicloWhile/src/ejemplociclowhile/Ejemplo051.java
+++ ejemplos-while/EjemploCicloWhile/src/ejemplociclowhile/Ejemplo051.java
@@ -13,40 +13,41 @@ import java.util.Scanner;
  * @author reroes
  */
 public class Ejemplo051 {
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        
+
         Scanner entrada = new Scanner(System.in);
         entrada.useLocale(Locale.US);
-        
+
         int limite = 3;
         int contador = 1;
         double suma_total = 0;
-        
+
         double promedio_final;
         double calificacion;
         String mensajefinal = "";
-        
+
         System.out.println("Ingrese las notas de los estudiantes de su "
                 + "materia");
-        
-        while (contador <= limite){
+
+        while (contador <= limite) {
             System.out.printf("Ingrese calificación número %d\n", contador);
             calificacion = entrada.nextDouble();
             suma_total = suma_total + calificacion;
             mensajefinal = String.format("%snota ingresada: %f\n",
-                    mensajefinal,calificacion);
+                    mensajefinal, calificacion);
             // contador
             contador = contador + 1;
         }
-        
-        promedio_final = suma_total/limite;
+
+        promedio_final = suma_total / limite;
         mensajefinal = String.format("%s\nEl promedio final es: %f",
-                    mensajefinal,promedio_final);
-        System.out.printf("%s\n",mensajefinal);
+                mensajefinal, promedio_final);
+        System.out.printf("%s\n", mensajefinal);
 //        System.out.printf("El promedio final es %f\n", promedio_final);
     }
 }
